3e118795003f37e8a7941f8cd5e2dedd1ba4adef
tahasama <taha.maatof@gmail.com>
2022-08-31 15:38:42 +0100

    uninstall and delete animation with framer

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Home from "./components/home";
 import Navbar from "./components/navbar";
 import Portfolio from "./components/portfolio";
 import Social from "./components/social";
-import { motion } from "framer-motion";
 import React, { useState } from "react";
 
 function App() {
